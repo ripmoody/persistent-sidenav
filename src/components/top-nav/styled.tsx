74ca8b0d3464d431a867b244719a8888106f375d
ripmoody <ripplingmoody@gmail.com>
2023-04-26 13:19:36 -0700

    update

--- src/components/top-nav/styled.tsx
+++ src/components/top-nav/styled.tsx
@@ -7,5 +7,5 @@ export const TopNav = styled.nav`
   padding: var(--space-1) var(--space-3);
   background-color: var(--color-plum-700);
   color: white;
-  gap: var(--space-3);
+  gap: var(--space-6);
 `;
