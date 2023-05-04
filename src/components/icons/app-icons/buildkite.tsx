99c776bc6a25b680222c9a4813deed870bcf3e60
ripmoody <ripplingmoody@gmail.com>
2023-05-04 15:24:09 -0700

    progress

---
+++ src/components/icons/app-icons/buildkite.tsx
@@ -0,0 +1,17 @@
+import { createIcon } from '@/components/icons/create-icon'
+
+export const Buildkite = createIcon({
+  path: (
+    <>
+      <rect width="24" height="24" rx="3.6" fill="white" />
+      <path
+        d="M3 6.8999V12.9067L8.95568 15.9101V9.90329L3 6.8999ZM14.9193 6.8999V12.9067L20.875 9.90329L14.9193 6.8999Z"
+        fill="#30F2A2"
+      />
+      <path
+        d="M14.9208 12.8543V18.8166L20.8752 15.8354V9.88106L14.9208 12.8543V12.8543ZM8.9585 9.88106V15.8434L14.9128 12.8622V6.8999L8.9585 9.88106Z"
+        fill="#14CC80"
+      />
+    </>
+  ),
+})
---
+++ src/components/icons/app-icons/microsoft.tsx
@@ -0,0 +1,22 @@
+import { createIcon } from '@/components/icons/create-icon'
+
+export const Microsoft = createIcon({
+  path: (
+    <>
+      <rect width="24" height="24" rx="3.6" fill="white" />
+      <path d="M5.1001 5.1001H11.6762V11.6762H5.1001V5.1001Z" fill="#F35325" />
+      <path
+        d="M12.3335 5.1001H18.9096V11.6762H12.3335V5.1001Z"
+        fill="#81BC06"
+      />
+      <path
+        d="M5.1001 12.3337H11.6762V18.9098H5.1001V12.3337Z"
+        fill="#05A6F0"
+      />
+      <path
+        d="M12.3335 12.3337H18.9096V18.9098H12.3335V12.3337Z"
+        fill="#FFBA08"
+      />
+    </>
+  ),
+})
